6e56f02284c7816c710e48102e82ed5acb8ea9e3
Spencer Finnell <spencerfinnell@gmail.com>
2015-09-08 17:36:21 -0500

    strings

--- src/templates/dashboard-login.php
+++ src/templates/dashboard-login.php
@@ -1,1 +1,5 @@
-<a href="<?php echo wp_login_url( donkey_get_page_url( 'licenses' ) ); ?> "><?php _e( 'Please log in to continue.', 'donkey' ); ?></a>
+<?php printf( 
+	__( '<a href="%s" class="button">Log in to Continue</a> | <a href="%s" class="button">Register a New Account</a>', 'donkey' ),
+	wp_login_url( donkey_get_page_url( 'licenses' ) ),
+	wp_registration_url()
+); ?>
