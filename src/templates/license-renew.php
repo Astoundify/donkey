74828e2f191cc773f0bcbe46af3ff0139de588a4
Spencer Finnell <spencerfinnell@gmail.com>
2015-09-09 10:32:22 -0400

    string

--- src/templates/license-renew.php
+++ src/templates/license-renew.php
@@ -1,5 +1,5 @@
 <p><a href="<?php echo donkey_get_page_url( 'licenses' ); ?>"><?php _e( '&larr; Manage Licenses', 'donkey' ); ?></a></p>
-<p>Still need support? <a href="<?php echo $license->get_item_url(); ?>?ref=Astoundify">Visit the item page</a> to renew your support. Once completed, enter your license key below.</p>
+<p><?php printf( __( 'Still need support? <a href="%s">Visit the item page</a> to renew your support. Once completed, enter your license key below.</p>', 'donkey' ), esc_url( add_query_arg( 'ref', 'Astoundify', $license->get_item_url() ) ) ); ?>
 
 <form action="" method="POST">
 	<p>
