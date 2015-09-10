a51e98820cf54ceee96042ebfb2260049ae7b23a
Spencer Finnell <spencerfinnell@gmail.com>
2015-09-10 13:43:46 -0400

    Clarify message.

--- src/templates/oauth.php
+++ src/templates/oauth.php
@@ -1,2 +1,3 @@
 <p><?php _e( 'Hey there! Before you can submit a ticket you must first connect your Envato account and add a valid license code.', 'donkey' ); ?></p>
+<p><?php _e( 'Connected before? No worries! Just click below again and your existing licenses will still be available', 'donkey' ); ?></p>
 <p><strong><a href="<?php echo donkey()->oauth->auth_url(); ?>" class="button"><?php _e( 'Connect to Envato', 'donkey' ); ?></a></strong></p>
