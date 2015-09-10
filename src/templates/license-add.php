3b3120aba94064fc53acb1678d8c59312c35be63
Spencer Finnell <spencerfinnell@gmail.com>
2015-09-10 10:48:33 -0400

    instructions. Closes #19

--- src/templates/license-add.php
+++ src/templates/license-add.php
@@ -1,8 +1,9 @@
 <p><a href="<?php echo donkey_get_page_url( 'licenses' ); ?>"><?php _e( '&larr; Manage Licenses', 'donkey' ); ?></a></p>
+<p><a href="https://help.market.envato.com/hc/en-us/articles/202822600-Where-Can-I-Find-my-Purchase-Code-"><?php _e( 'Where can I find my purchase code?', 'donkey' ); ?></a></p>
 
 <form action="" method="POST">
     <p>
-        <label for="code"><?php _e( 'License Code', 'donkey' ); ?></label>
+        <label for="code"><?php _e( 'Purchase Code', 'donkey' ); ?></label>
         <input type="text" name="purchase-key" class="regular-text" placeholder="" />
     </p>
     <p>
--- src/templates/license-renew.php
+++ src/templates/license-renew.php
@@ -1,9 +1,10 @@
 <p><a href="<?php echo donkey_get_page_url( 'licenses' ); ?>"><?php _e( '&larr; Manage Licenses', 'donkey' ); ?></a></p>
-<p><?php printf( __( 'Still need support? <a href="%s">Visit the item page</a> to renew your support. Once completed, enter your license key below.</p>', 'donkey' ), esc_url( add_query_arg( 'ref', 'Astoundify', $license->get_item_url() ) ) ); ?>
+<p><?php printf( __( 'Still need support? <a href="%s">Visit the item page</a> to renew your support. Once completed, enter your purchase code below.</p>', 'donkey' ), esc_url( add_query_arg( 'ref', 'Astoundify', $license->get_item_url() ) ) ); ?>
+<p><a href="https://help.market.envato.com/hc/en-us/articles/202822600-Where-Can-I-Find-my-Purchase-Code-"><?php _e( 'Where can I find my purchase code?', 'donkey' ); ?></a></p>
 
 <form action="" method="POST">
 	<p>
-		<label for="code"><?php _e( 'License Code', 'donkey' ); ?></label>
+		<label for="code"><?php _e( 'Purchase Code', 'donkey' ); ?></label>
 		<input type="text" name="purchase-key" class="regular-text" placeholder="" />
 	</p>
 	<p>
