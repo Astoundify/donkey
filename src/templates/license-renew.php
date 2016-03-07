f2d298b6db85b3d8036ec2085e94bcc14f5f468d
Spencer Finnell <spencerfinnell@gmail.com>
2016-03-07 13:46:34 -0500

    Better instructions on renewing support on the form.

--- src/templates/license-renew.php
+++ src/templates/license-renew.php
@@ -1,6 +1,7 @@
 <p><a href="<?php echo donkey_get_page_url( 'licenses' ); ?>"><?php _e( '&larr; Manage Licenses', 'donkey' ); ?></a></p>
-<p><?php printf( __( 'Still need support? <a href="%s">Visit the item page</a> to renew your support. Once completed, enter your purchase code below.</p>', 'donkey' ), esc_url( add_query_arg( 'ref', 'Astoundify', $license->get_item_url() ) ) ); ?>
-<p><a href="https://help.market.envato.com/hc/en-us/articles/202822600-Where-Can-I-Find-my-Purchase-Code-"><?php _e( 'Where can I find my purchase code?', 'donkey' ); ?></a></p>
+<div class="edd-alert edd-alert-info">
+	<p><?php printf( __( 'Still need support? <a href="%s">Visit the item page</a> to renew your support. Once completed, enter your purchase code below.</p>', 'donkey' ), esc_url( add_query_arg( 'ref', 'Astoundify', $license->get_item_url() ) ) ); ?>. <a href="https://help.market.envato.com/hc/en-us/articles/202822600-Where-Can-I-Find-my-Purchase-Code-"><?php _e( 'Where can I find my purchase code?', 'donkey' ); ?></a></p>
+</div>
 
 <form action="" method="POST">
 	<p>
