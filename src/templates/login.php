9c1d405185b4deaca86826e6802d35c9ac890902
Spencer Finnell <spencerfinnell@gmail.com>
2016-03-14 13:07:10 -0400

    Use EDD login form on login notice.

--- src/templates/login.php
+++ src/templates/login.php
@@ -1,5 +1,5 @@
-<?php printf( 
-	__( '<a href="%s" class="button">Log in to Continue</a> <a href="%s" class="button">Register a New Account</a>', 'donkey' ),
-	wp_login_url( donkey_get_page_url( 'licenses' ) ),
-	wp_registration_url()
-); ?>
+<p class="edd-alert edd-alert-warn">
+	<?php _e( 'You must be logged in to view license keys.', 'donkey' ); ?>
+</p>
+
+<?php echo edd_login_form(); ?>
